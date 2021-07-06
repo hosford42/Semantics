b8f1b97bdd63bb32b023a8915164b8b8da5a8b9a
Aaron Hosford <hosford42@gmail.com>
2021-07-05 21:13:00 -0500

    Refactored unit tests to bettera align with package structure

---
+++ test_semantics/test_kb_layer/test_schema_attributes.py
@@ -0,0 +1,94 @@
+from unittest import TestCase
+
+
+class TestFunctions(TestCase):
+
+    def test_default_attribute_preference(self):
+        self.fail()
+
+
+class TestAttributeDescriptor(TestCase):
+
+    def test_property_descriptor_protocol(self):
+        self.fail()
+
+    def test_preference(self):
+        self.fail()
+
+    def test_validation(self):
+        self.fail()
+
+    def test_iter_choices(self):
+        self.fail()
+
+    def test_best_choice(self):
+        self.fail()
+
+    def test_sorted_choices(self):
+        self.fail()
+
+
+class TestSingularAttribute(TestCase):
+
+    def test_callable_protocol(self):
+        self.fail()
+
+    def test_defined(self):
+        self.fail()
+
+
+class TestSingularAttributeDescriptor(TestCase):
+
+    def test_property_descriptor_protocol(self):
+        self.fail()
+
+    def test_defined(self):
+        self.fail()
+
+    def test_get_value(self):
+        self.fail()
+
+
+class TestPluralAttribute(TestCase):
+
+    def test_len(self):
+        self.fail()
+
+    def test_contains(self):
+        self.fail()
+
+    def test_add(self):
+        self.fail()
+
+    def test_remove(self):
+        self.fail()
+
+    def test_discard(self):
+        self.fail()
+
+    def test_callable_protocol(self):
+        self.fail()
+
+
+class TestPluralAttributeDescriptor(TestCase):
+
+    def test_property_descriptor_protocol(self):
+        self.fail()
+
+    def test_count(self):
+        self.fail()
+
+    def test_iter_values(self):
+        self.fail()
+
+    def test_add(self):
+        self.fail()
+
+    def test_remove(self):
+        self.fail()
+
+    def test_discard(self):
+        self.fail()
+
+    def test_contains(self):
+        self.fail()
